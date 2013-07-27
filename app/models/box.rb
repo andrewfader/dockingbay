9ec46f3bd57acea6603e1f615646aad513e55627
Andrew Fader <andrevan@gmail.com>
2013-07-27 04:02:36 -0700

    links, etc

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,4 +1,5 @@
 class Box < ActiveRecord::Base
+  include ActionView::Helpers::DateHelper
   belongs_to :user
 
   def container
@@ -7,5 +8,13 @@ class Box < ActiveRecord::Base
 
   def ports
     JSON.pretty_generate(container.networksettings.PortMapping.Tcp.attributes).to_s.gsub(/[\"\{\}]/,"").gsub(":"," <- ").gsub(",",", ") if container.try(:networksettings).try(:PortMapping)
+  end
+
+  def status
+    if container.state.Running
+      "Up for #{time_ago_in_words(container.state.StartedAt)}"
+    else
+      "Down (exit #{container.state.ExitCode})"
+    end
   end
 end
