95a0ca8629ea0bee3f11dcbb47f97c08147d591a
Andrew Fader <andrevan@gmail.com>
2013-07-29 15:30:15 -0700

    only reload container logs when empty or manually triggered

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -3,7 +3,15 @@ class ContainersController < InheritedResources::Base
   actions :index, :show
   def show
     @container = Container.find(params[:id])
-    Resque.enqueue(StreamContainerDataJob, {container_id: @container.id, container_data_id: @container.data.id})
+    unless @container.data.data.present?
+      Resque.enqueue(StreamContainerDataJob, {container_id: @container.id, container_data_id: @container.data.id})
+    end
     @output = @container.data.data
+  end
+
+  def refresh
+    @container = Container.find(params[:container_id])
+    Resque.enqueue(StreamContainerDataJob, {container_id: @container.id, container_data_id: @container.data.id})
+    redirect_to container_path(@container)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Dockingbay::Application.routes.draw do
   devise_for :users, controllers: {sessions: "sessions"}
-  resources :containers, only: [:index, :show]
+  resources :containers, only: [:index, :show] do
+    get :refresh
+  end
   resources :images, only: :index
   resources :boxes
   root to: 'welcome#index'
